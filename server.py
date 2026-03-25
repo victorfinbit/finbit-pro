016031860bb4ec5d3c31fe83b0dc205decf8f8de
VICTOR MANUEL RODRIGUEZ ALVAREZ <DGPVRODRIGUEZAL@guanajuato.gob.mx>
2026-03-25 02:18:50 -0600

    fix render port

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_file
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def dashboard():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
