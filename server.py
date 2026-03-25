5ffb9b15b47334195920e4ded4ef4ce8f66bdb83
victorfinbit <vrodriguezalvarez5@gmail.com>
2026-03-25 10:53:08 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,12 +3,10 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def dashboard():
     return send_file("dashboard.html")
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 1000))
     app.run(host="0.0.0.0", port=port)
